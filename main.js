c3748edfcc0bd6e9089d20f443151641e4806a72
Christoph Friedrich <christoph.friedrich@vonaffenfels.de>
2016-01-27 13:28:02 +0100

    Initial Skeleton commit

---
+++ main.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// @IMPORTS
+var Application = require('./lib/Application');
+
+Application.configure({
+    rootDir: __dirname,
+    modules_path: __dirname + '/modules',
+    config_path: __dirname + '/config',
+    stage: process.env.STAGE || process.env.NODE_ENV || 'dev',
+    logformat: 'DD.MM.YYYY h:mm:ss',
+    logDir: __dirname + '/logs',
+    stages: [
+        'dev',
+        'view',
+        'prod'
+    ]
+});
+
+// resources
+Application.registerModule('webserver');
+Application.registerModule('sockets');
+Application.registerModule('database');
+
+Application.run();
+
+process.on('exit', function () {
+    Application.stop();
+});
